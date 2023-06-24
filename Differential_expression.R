e49f7f6fc8c68ea483bc47c245da3fa4d520892d
Sandra Costa <sanralisistrata@gmail.com>
2023-06-24 10:43:50 -0400

    Reset index & remove samples

--- Differential_expression.R
+++ Differential_expression.R
@@ -1,11 +1,27 @@
 #load libraries
 ##########################
 library(DESeq2)
-library(airway)
-library(readxl)
+library(TCGAbiolinks)
 #########################
 
 #Preparing count data
 ########################
 setwd("/home/sam/Documents/Melanoma")
-read_excel("sample.xls")
+
+##### LOCAL DATA #############
+#Read samples information
+info <- read.csv("sample.csv")
+
+# Remove unnecessary samples
+sample <- info[!(info$`Sample.Type` == 'Solid Tissue Normal' | 
+                   info$`Sample.Type` == 'Additional Metastatic'), ]
+# Reset index
+sample <- sample[order(row.names(sample)),]
+rownames(sample) <- NULL  
+
+
+
+
+
+
+
